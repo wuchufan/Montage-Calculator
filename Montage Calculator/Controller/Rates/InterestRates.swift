161c261fc02972d2090ae2143dcf49e383ffa8c4
TIEC <TIEC@infos-iMac.local>
2019-04-11 15:44:06 -0400

    added swift file for interestrate, created ideal model for each rate cell

---
+++ Montage Calculator/Controller/Rates/InterestRates.swift
@@ -0,0 +1,34 @@
+//
+//  InterestRates.swift
+//  Montage Calculator
+//
+//  Created by TIEC on 2019-04-11.
+//  Copyright © 2019 TIEC. All rights reserved.
+//
+
+import UIKit
+
+class InterestRates: UIViewController {
+
+
+    @IBAction func toCalculator(_ sender: Any) {
+        print("it works")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
